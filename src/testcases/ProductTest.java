d057c4bf798f3c09ecef9f35c03d2a34f00a2b49
MomenBostami <125808874+MomenBostami@users.noreply.github.com>
2023-05-16 12:15:15 +0300

    clean

--- src/testcases/ProductTest.java
+++ src/testcases/ProductTest.java
@@ -8,25 +8,21 @@ import model.Product;
 
 public class ProductTest {
 
-	@Test
 	public void testGetId() {
 		Product product = new Product(1, "Carpet Cleaning", 50.0);
 		assertEquals(1, product.getId());
 	}
 
-	@Test
 	public void testGetName() {
 		Product product = new Product(1, "Carpet Cleaning", 50.0);
 		assertEquals("Carpet Cleaning", product.getName());
 	}
 	
-	@Test
 	public void testGetPrice() {
 		Product product = new Product(1, "Carpet Cleaning", 50.0);
 		assertEquals(50.0, product.getPrice(), 0.01);
 	}
 
-	@Test
 	public void testSetPrice() {
 		Product product = new Product(1, "Carpet Cleaning", 50.0);
 		product.setPrice(60.0);
